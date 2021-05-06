df497abb8b07fe18b2438b27c9f565a2b283ec32
Hitendra Solanki <hitendra.developer@gmail.com>
2021-05-06 16:14:52 +0530

    removed pod install directory for example

--- Example/Pods/Target Support Files/HSCenterSlider/HSCenterSlider-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HSCenterSlider : NSObject
-@end
-@implementation PodsDummy_HSCenterSlider
-@end
--- Example/Pods/Target Support Files/HSRange/HSRange-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HSRange : NSObject
-@end
-@implementation PodsDummy_HSRange
-@end
